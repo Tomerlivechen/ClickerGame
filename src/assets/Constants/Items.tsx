3800c36f882c818a8cb942d6cec15790c7144adc
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2024-12-31 21:26:51 +0200

    added items

--- src/assets/Constants/Items.tsx
+++ src/assets/Constants/Items.tsx
@@ -1,393 +1,394 @@
 import { itemVals } from "../Types/ButtonTypes";
 import { SpecialNames } from "./Special";
 
+const Lepton: itemVals = {
+  Id: "",
+  name: "Lepton",
+  price: 9n,
+  visibleValue: 0n,
+  PriceIncrease: 0.05,
+  PayOff: 1n,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
+const Boson: itemVals = {
+  Id: "",
+  name: "Boson",
+  price: 100n,
+  visibleValue: 0n,
+  PriceIncrease: 0.05,
+  PayOff: 10n,
+  RequiredSpecial: SpecialNames.WeakForce,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Lepton : itemVals = {
-    Id: "",
-    name: "Lepton",
-    price: 9n,
-    visibleValue: 0n,
-    PriceIncrease: 0.05,
-    PayOff: 1n,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Atoms: itemVals = {
+  Id: "",
+  name: "Atoms",
+  price: 3000n,
+  visibleValue: 0n,
+  PriceIncrease: 0.05,
+  PayOff: 150n,
+  RequiredSpecial: SpecialNames.StringForce,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Boson : itemVals = {
-    Id: "",
-    name: "Boson",
-    price: 100n,
-    visibleValue: 0n,
-    PriceIncrease: 0.05,
-    PayOff: 10n,
-    RequiredSpecial: SpecialNames.WeakForce,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Gas: itemVals = {
+  Id: "",
+  name: "Gas",
+  price: 120000n,
+  visibleValue: 480000n,
+  PriceIncrease: 0.05,
+  PayOff: 4800n,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Atoms : itemVals = {
-    Id: "",
-    name: "Atoms",
-    price: 3000n,
-    visibleValue: 0n,
-    PriceIncrease: 0.05,
-    PayOff: 150n,
-    RequiredSpecial: SpecialNames.StringForce,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Dust: itemVals = {
+  Id: "",
+  name: "Dust",
+  price: 4800000n,
+  visibleValue: 1920000n,
+  PriceIncrease: 0.05,
+  PayOff: 384000n,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Gas : itemVals = {
-    Id: "",
-    name: "Gas",
-    price: 120000n,
-    visibleValue: 480000n,
-    PriceIncrease: 0.05,
-    PayOff: 4800n,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Water: itemVals = {
+  Id: "",
+  name: "Water",
+  price: 96000000n,
+  visibleValue: 38400000n,
+  PriceIncrease: 0.05,
+  PayOff: 7680000n,
+  RequiredSpecial: SpecialNames.Chemistry,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Dust : itemVals = {
-    Id: "",
-    name: "Dust",
-    price: 4800000n,
-    visibleValue: 1920000n,
-    PriceIncrease: 0.05,
-    PayOff: 384000n,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Rocks: itemVals = {
+  Id: "",
+  name: "Rocks",
+  price: 1920000000n,
+  visibleValue: 768000000n,
+  PriceIncrease: 0.05,
+  PayOff: 172800000n,
+  RequiredSpecial: SpecialNames.Electromagnetism,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Water : itemVals = {
-    Id: "",
-    name: "Water",
-    price: 96000000n,
-    visibleValue: 38400000n,
-    PriceIncrease: 0.05,
-    PayOff: 7680000n,
-    RequiredSpecial: SpecialNames.Chemistry,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Astroids: itemVals = {
+  Id: "",
+  name: "Astroids",
+  price: 115200000000n,
+  visibleValue: 46080000000n,
+  PriceIncrease: 0.05,
+  PayOff: 3456000000n,
+  RequiredSpecial: SpecialNames.Gravity,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Rocks : itemVals = {
-    Id: "",
-    name: "Rocks",
-    price: 1920000000n,
-    visibleValue: 768000000n,
-    PriceIncrease: 0.05,
-    PayOff: 172800000n,
-    RequiredSpecial: SpecialNames.Electromagnetism,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Commet: itemVals = {
+  Id: "",
+  name: "Commet",
+  price: 9216000000000n,
+  visibleValue: 3686400000000n,
+  PriceIncrease: 0.05,
+  PayOff: 737280000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Astroids : itemVals = {
-    Id: "",
-    name: "Astroids",
-    price: 115200000000n,
-    visibleValue: 46080000000n,
-    PriceIncrease: 0.05,
-    PayOff: 3456000000n,
-    RequiredSpecial: SpecialNames.Gravity,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const DwarfPlanet: itemVals = {
+  Id: "",
+  name: "Dwarf Planet",
+  price: 737280000000000n,
+  visibleValue: 294912000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 58982400000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const Commet : itemVals = {
-    Id: "",
-    name: "Commet",
-    price: 9216000000000n,
-    visibleValue: 3686400000000n,
-    PriceIncrease: 0.05,
-    PayOff: 737280000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const RockyPlanet: itemVals = {
+  Id: "",
+  name: "Rocky Planet",
+  price: 14745600000000000n,
+  visibleValue: 5898240000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 1179648000000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const DwarfPlanet : itemVals = {
-    Id: "",
-    name: "Dwarf Planet",
-    price: 737280000000000n,
-    visibleValue: 294912000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 58982400000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const IcePlanet: itemVals = {
+  Id: "",
+  name: "IcePlanet",
+  price: 589824000000000000n,
+  visibleValue: 235929600000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 29491200000000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const RockyPlanet : itemVals = {
-    Id: "",
-    name: "Rocky Planet",
-    price: 14745600000000000n,
-    visibleValue: 5898240000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 1179648000000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const GasPlanet: itemVals = {
+  Id: "",
+  name: "GasPlanet",
+  price: 53084160000000000000n,
+  visibleValue: 21233664000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 4246732800000000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const IcePlanet : itemVals = {
-    Id: "",
-    name: "IcePlanet",
-    price: 589824000000000000n,
-    visibleValue: 235929600000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 29491200000000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const BrownDwarf: itemVals = {
+  Id: "",
+  name: "Brown Dwarf",
+  price: 530841600000000000000n,
+  visibleValue: 212336640000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 5308416000000000000n,
+  StarEffect: 2.2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const GasPlanet : itemVals = {
-    Id: "",
-    name: "GasPlanet",
-    price: 53084160000000000000n,
-    visibleValue: 21233664000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 4246732800000000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarM: itemVals = {
+  Id: "",
+  name: "M type Star",
+  price: 5308416000000000000000n,
+  visibleValue: 2123366400000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 318504960000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const BrownDwarf : itemVals = {
-    Id: "",
-    name: "Brown Dwarf",
-    price: 530841600000000000000n,
-    visibleValue: 212336640000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 5308416000000000000n,
-    StarEffect:2.2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarK: itemVals = {
+  Id: "",
+  name: "K type Star",
+  price: 212336640000000000000000n,
+  visibleValue: 84934656000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 16986931200000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarM : itemVals = {
-    Id: "",
-    name: "M type Star",
-    price: 5308416000000000000000n,
-    visibleValue: 2123366400000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 318504960000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarG: itemVals = {
+  Id: "",
+  name: "G type Star",
+  price: 8493465600000000000000000n,
+  visibleValue: 3397386240000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 424673280000000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarK : itemVals = {
-    Id: "",
-    name: "K type Star",
-    price: 212336640000000000000000n,
-    visibleValue: 84934656000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 16986931200000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarF: itemVals = {
+  Id: "",
+  name: "F type Star",
+  price: 339738624000000000000000000n,
+  visibleValue: 135895449600000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 3397386240000000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarG : itemVals = {
-    Id: "",
-    name: "G type Star",
-    price: 8493465600000000000000000n,
-    visibleValue: 3397386240000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 424673280000000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarA: itemVals = {
+  Id: "",
+  name: "A type Star",
+  price: 3397386240000000000000000000n,
+  visibleValue: 1358954496000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 203843174400000000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarF : itemVals = {
-    Id: "",
-    name: "F type Star",
-    price: 339738624000000000000000000n,
-    visibleValue: 135895449600000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 3397386240000000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarB: itemVals = {
+  Id: "",
+  name: "B type Star",
+  price: 135895449600000000000000000000n,
+  visibleValue: 54358179840000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 2717908992000000000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarA : itemVals = {
-    Id: "",
-    name: "A type Star",
-    price: 3397386240000000000000000000n,
-    visibleValue: 1358954496000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 203843174400000000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const StarO: itemVals = {
+  Id: "",
+  name: "O type Star",
+  price: 5435817984000000000000000000000n,
+  visibleValue: 2174327193600000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 326149079040000000000000000000n,
+  RequiredSpecial: SpecialNames.Fusion,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarB : itemVals = {
-    Id: "",
-    name: "B type Star",
-    price: 135895449600000000000000000000n,
-    visibleValue: 54358179840000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 2717908992000000000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const NurtonStar: itemVals = {
+  Id: "",
+  name: "Nurton Star",
+  price: 163074539520000000000000000000000n,
+  visibleValue: 65229815808000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 13045963161600000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity2,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const StarO : itemVals = {
-    Id: "",
-    name: "O type Star",
-    price: 5435817984000000000000000000000n,
-    visibleValue: 2174327193600000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 326149079040000000000000000000n,
-    RequiredSpecial: SpecialNames.Fusion,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const BlackHole: itemVals = {
+  Id: "",
+  name: "Black Hole",
+  price: 1630745395200000000000000000000000n,
+  visibleValue: 652298158080000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 48922361856000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-const NurtonStar : itemVals = {
-    Id: "",
-    name: "Nurton Star",
-    price: 163074539520000000000000000000000n,
-    visibleValue: 65229815808000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 13045963161600000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity2,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
-
-
-const BlackHole : itemVals = {
-    Id: "",
-    name: "Black Hole",
-    price: 1630745395200000000000000000000000n,
-    visibleValue: 652298158080000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 48922361856000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
-
-const Quasar : itemVals = {
-    Id: "",
-    name: "Quasar",
-    price: 32614907904000000000000000000000000n,
-    visibleValue: 13045963161600000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 1956894474240000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+const Quasar: itemVals = {
+  Id: "",
+  name: "Quasar",
+  price: 32614907904000000000000000000000000n,
+  visibleValue: 13045963161600000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 1956894474240000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
 const Nebula: itemVals = {
-    Id: "",
-    name: "Nebula",
-    price: 652298158080000000000000000000000000n,
-    visibleValue: 260919263232000000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 32614907904000000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
-
+  Id: "",
+  name: "Nebula",
+  price: 652298158080000000000000000000000000n,
+  visibleValue: 260919263232000000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 32614907904000000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
 const Galaxy: itemVals = {
-    Id: "",
-    name: "Galaxy",
-    price: 39137889484800000000000000000000000000n,
-    visibleValue: 15655155793920000000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 3131031158784000000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
-
+  Id: "",
+  name: "Galaxy",
+  price: 39137889484800000000000000000000000000n,
+  visibleValue: 15655155793920000000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 3131031158784000000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
 const GalexyCluster: itemVals = {
-    Id: "",
-    name: "Galexy Cluster",
-    price: 1174136684544000000000000000000000000000n,
-    visibleValue: 469654673817600000000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 46965467381760000000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+  Id: "",
+  name: "Galexy Cluster",
+  price: 1174136684544000000000000000000000000000n,
+  visibleValue: 469654673817600000000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 46965467381760000000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
 const Universe: itemVals = {
-    Id: "",
-    name: "Universe",
-    price: 58706834227200000000000000000000000000000n,
-    visibleValue: 23482733690880000000000000000000000000000n,
-    PriceIncrease: 0.05,
-    PayOff: 587068342272000000000000000000000000000n,
-    RequiredSpecial: SpecialNames.Gravity3,
-    ActiveImage: "",
-    HiddenImage: "",
-    invisibleImage: ""
-}
+  Id: "",
+  name: "Universe",
+  price: 58706834227200000000000000000000000000000n,
+  visibleValue: 23482733690880000000000000000000000000000n,
+  PriceIncrease: 0.05,
+  PayOff: 587068342272000000000000000000000000000n,
+  RequiredSpecial: SpecialNames.Gravity3,
+  ActiveImage: "",
+  HiddenImage: "",
+  invisibleImage: "",
+};
 
-export const UniverseItems = {Lepton,Boson,Atoms,Gas,Dust,Water,
-    Rocks,
-    Astroids,
-    Commet,
-    DwarfPlanet,
-    RockyPlanet,
-    IcePlanet,
-    GasPlanet,
-    BrownDwarf,
-    StarM,
-    StarK,
-    StarG,
-    StarF,
-    StarA,
-    StarB,
-    StarO,
-    NurtonStar,
-    BlackHole,
-    Quasar,
-    Nebula,
-    Galaxy,
-    GalexyCluster,
-    Universe,
-    }
+export const UniverseItems = {
+  Lepton,
+  Boson,
+  Atoms,
+  Gas,
+  Dust,
+  Water,
+  Rocks,
+  Astroids,
+  Commet,
+  DwarfPlanet,
+  RockyPlanet,
+  IcePlanet,
+  GasPlanet,
+  BrownDwarf,
+  StarM,
+  StarK,
+  StarG,
+  StarF,
+  StarA,
+  StarB,
+  StarO,
+  NurtonStar,
+  BlackHole,
+  Quasar,
+  Nebula,
+  Galaxy,
+  GalexyCluster,
+  Universe,
+};
--- src/assets/ContextAPI/InventoryContext.tsx
+++ src/assets/ContextAPI/InventoryContext.tsx
@@ -1,115 +1,227 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { StoredInventory } from "../Types/ButtonTypes";
+import {
+  Dimension,
+  Finger,
+  itemAmount,
+  itemVals,
+  Special,
+  StoredInventory,
+} from "../Types/ButtonTypes";
 import { DecodeStorage, EncodeStorage } from "../Constants/Methods";
 
 const initialValues = {
-    InventoryString: localStorage.getItem("ClickerInventory")?.toString()?? null ,
-    Enthalpy : localStorage.getItem("ClickerEnthalpy")?.toString()?? null
-  };
-  
-const  BaseInventoryString : StoredInventory = {
+  InventoryString: localStorage.getItem("ClickerInventory")?.toString() ?? null,
+  Enthalpy: localStorage.getItem("ClickerEnthalpy")?.toString() ?? null,
+};
+
+const BaseInventoryString: StoredInventory = {
   ItemList: [],
   SpecialList: [],
-  FingerList: [{
-    Id: "",
-    name: "Finger1",
-    price: 0n,
-    PayOff: 1,
-    ActiveImage: "",
-    HiddenImage: ""
-  }],
+  FingerList: [
+    {
+      Id: "",
+      name: "Finger1",
+      price: 0n,
+      PayOff: 1,
+      ActiveImage: "",
+      HiddenImage: "",
+    },
+  ],
   DimensionList: [],
-  HasStar: false
-} 
-
+  HasStar: false,
+};
 
 const defaultValues = {
   DefaultInventory: BaseInventoryString,
-  DefaultEnthalpy : 0n
+  DefaultEnthalpy: 0n,
 };
 
+const InventoryContext = createContext<{
+  inventory: StoredInventory | undefined;
+  enthalpy: bigint;
+  clickOn: () => void;
+  addItem: (item: itemVals) => void;
+  addSpecial: (special: Special) => void;
+  Reset: (NewDimention: Dimension) => void;
+  addFinger: (finger: Finger) => void;
+  useEnthalpy: (amount: bigint) => void;
+}>({
+  inventory: undefined,
+  enthalpy: 0n,
+  clickOn: () => {},
+  addItem: () => {},
+  addSpecial: () => {},
+  Reset: () => {},
+  addFinger: () => {},
+  useEnthalpy: () => {},
+});
 
-  const InventoryContext = createContext<{
-    inventory: StoredInventory | undefined;
-    enthalpy: bigint;
-    clickOn: () => void;
-  }>({
-    inventory: undefined,
-    enthalpy: 0n,
-    clickOn: () => {},
-  });
-  
-  const InventoryProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+const InventoryProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [inventory, setInventory] = useState<StoredInventory>(
+    defaultValues.DefaultInventory
+  );
+  const [enthalpy, setEnthalpy] = useState<bigint>(
+    defaultValues.DefaultEnthalpy
+  );
 
-    const [inventory, setInventory] = useState<StoredInventory>(defaultValues.DefaultInventory);
-    const [enthalpy, setEnthalpy] = useState<bigint>(defaultValues.DefaultEnthalpy);
-
-    useEffect(() =>{
-      if (initialValues.Enthalpy){
-        setEnthalpy(BigInt(initialValues.Enthalpy))
-      }
-else {
-  setEnthalpy(defaultValues.DefaultEnthalpy)
-}
-      if(initialValues.InventoryString) {
-        setInventory(DecodeStorage(initialValues.InventoryString))
-      }
-      else {
-        setInventory(defaultValues.DefaultInventory)
-      }
-    },[])
-
-    useEffect(() =>{
-      localStorage.setItem("ClickerEnthalpy",enthalpy.toString())
-    },[enthalpy])
-    useEffect(() =>{
-      if (inventory){
-      localStorage.setItem("ClickerInventory",EncodeStorage(inventory))
-      }
-    },[inventory])
-  
-    const clickOn = () => {
-      let fingerValue : bigint = 1n;
-      inventory?.FingerList.forEach(FingerList => {fingerValue = fingerValue * BigInt(FingerList.PayOff)} );
-      setEnthalpy((prev)=>prev + fingerValue);
+  useEffect(() => {
+    if (initialValues.Enthalpy) {
+      setEnthalpy(BigInt(initialValues.Enthalpy));
+    } else {
+      setEnthalpy(defaultValues.DefaultEnthalpy);
     }
-
-const AddEnthalpy = () =>{
-let AddEnthalpy : bigint = 0n;
-let MultiplyEnthalpy : bigint = 1n;
-if(inventory?.HasStar){
-inventory?.ItemList.forEach(ItemList => {AddEnthalpy = AddEnthalpy + BigInt(ItemList.Amount) * BigInt(ItemList.PayOff) * BigInt(ItemList.StarEffect ?? 1) } );
-}
-else {
-  if(inventory?.HasStar){
-    inventory?.ItemList.forEach(ItemList => {AddEnthalpy = AddEnthalpy + BigInt(ItemList.Amount) * BigInt(ItemList.PayOff)} );
+    if (initialValues.InventoryString) {
+      setInventory(DecodeStorage(initialValues.InventoryString));
+    } else {
+      setInventory(defaultValues.DefaultInventory);
     }
-}
-if (inventory?.DimensionList && inventory.DimensionList.length>0){
-  inventory?.DimensionList.forEach(DimensionList => {MultiplyEnthalpy = MultiplyEnthalpy * BigInt(DimensionList.PayOff) } );
-}
-if (inventory?.SpecialList && inventory.SpecialList.length>0){
-  inventory?.SpecialList.forEach(SpecialList => {MultiplyEnthalpy = MultiplyEnthalpy * BigInt(SpecialList.PayOff) } );
-}
-setEnthalpy((prev)=>prev + AddEnthalpy*MultiplyEnthalpy);
-}
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("ClickerEnthalpy", enthalpy.toString());
+  }, [enthalpy]);
+  useEffect(() => {
+    if (inventory) {
+      localStorage.setItem("ClickerInventory", EncodeStorage(inventory));
+    }
+  }, [inventory]);
 
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        AddEnthalpy();
-      }, 1000); 
-      return () => {
-        clearInterval(intervalId);
-      };
-    }, []);
-
-  
-    return (
-      <InventoryContext.Provider value={{ inventory, enthalpy, clickOn }}>
-        {children}
-      </InventoryContext.Provider>
-    );
+  const clickOn = () => {
+    let fingerValue: bigint = 1n;
+    inventory?.FingerList.forEach((FingerList) => {
+      fingerValue = fingerValue * BigInt(FingerList.PayOff);
+    });
+    setEnthalpy((prev) => prev + fingerValue);
   };
-  
-  export { InventoryContext, InventoryProvider };
+
+  const AddEnthalpy = () => {
+    let AddEnthalpy: bigint = 0n;
+    let MultiplyEnthalpy: bigint = 1n;
+    if (inventory?.HasStar) {
+      inventory?.ItemList.forEach((ItemList) => {
+        AddEnthalpy =
+          AddEnthalpy +
+          BigInt(ItemList.Amount) *
+            BigInt(ItemList.PayOff) *
+            BigInt(ItemList.StarEffect ?? 1);
+      });
+    } else {
+      if (inventory?.HasStar) {
+        inventory?.ItemList.forEach((ItemList) => {
+          AddEnthalpy =
+            AddEnthalpy + BigInt(ItemList.Amount) * BigInt(ItemList.PayOff);
+        });
+      }
+    }
+    if (inventory?.DimensionList && inventory.DimensionList.length > 0) {
+      inventory?.DimensionList.forEach((DimensionList) => {
+        MultiplyEnthalpy = MultiplyEnthalpy * BigInt(DimensionList.PayOff);
+      });
+    }
+    if (inventory?.SpecialList && inventory.SpecialList.length > 0) {
+      inventory?.SpecialList.forEach((SpecialList) => {
+        MultiplyEnthalpy = MultiplyEnthalpy * BigInt(SpecialList.PayOff);
+      });
+    }
+    setEnthalpy((prev) => prev + AddEnthalpy * MultiplyEnthalpy);
+  };
+
+  const addStar = () => {
+    if (!inventory.HasStar) {
+      setInventory((prev) => ({
+        ...prev,
+        HasStar: true,
+      }));
+    }
+  };
+
+  const addItem = (item: itemVals) => {
+    if (item.name.includes("Star") && !inventory.HasStar) {
+      addStar();
+    }
+    const existingItemIndex = inventory.ItemList.findIndex(
+      (i) => i.name === item.name
+    );
+    if (existingItemIndex !== -1) {
+      setInventory((prev) => {
+        const updatedItemList = [...prev.ItemList];
+        updatedItemList[existingItemIndex] = {
+          ...updatedItemList[existingItemIndex],
+          Amount: updatedItemList[existingItemIndex].Amount + 1n,
+        };
+
+        return {
+          ...prev,
+          ItemList: updatedItemList,
+        };
+      });
+    } else {
+      const newItem: itemAmount = {
+        name: item.name,
+        Amount: 1n,
+        PayOff: item.PayOff,
+      };
+
+      setInventory((prev) => ({
+        ...prev,
+        ItemList: [...prev.ItemList, newItem],
+      }));
+    }
+  };
+
+  const addSpecial = (special: Special) => {
+    setInventory((prev) => ({
+      ...prev,
+      SpecialList: [...prev.SpecialList, special],
+    }));
+  };
+
+  const addFinger = (Finger: Finger) => {
+    setInventory((prev) => ({
+      ...prev,
+      FingerList: [...prev.FingerList, Finger],
+    }));
+  };
+
+  const Reset = (NewDimention: Dimension) => {
+    setInventory((prev) => ({
+      ...BaseInventoryString,
+      DimensionList: [...prev.DimensionList, NewDimention],
+    }));
+  };
+
+  const useEnthalpy = (amount: bigint) => {
+    if (enthalpy > amount) {
+      const newEnthalpy = enthalpy - amount;
+      setEnthalpy(newEnthalpy);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      AddEnthalpy();
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <InventoryContext.Provider
+      value={{
+        inventory,
+        enthalpy,
+        clickOn,
+        addItem,
+        addSpecial,
+        Reset,
+        addFinger,
+        useEnthalpy,
+      }}
+    >
+      {children}
+    </InventoryContext.Provider>
+  );
+};
+
+export { InventoryContext, InventoryProvider };
