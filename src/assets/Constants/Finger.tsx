8fca38f519dbd8896d2c981ef5a75f2774964d25
Tomerlivechen <104824283+Tomerlivechen@users.noreply.github.com>
2025-01-04 20:02:54 +0200

    nothing

--- src/assets/Constants/Finger.tsx
+++ src/assets/Constants/Finger.tsx
@@ -5,7 +5,8 @@ const FingerPointer: Finger = {
   name: "Level 1",
   price: 1000n,
   PayOff: 2,
-  ActiveImage: "https://res.cloudinary.com/dhzndjkjl/image/upload/v1735814779/Finger1_hjg1ps.png",
+  ActiveImage:
+    "https://res.cloudinary.com/dhzndjkjl/image/upload/v1735814779/Finger1_hjg1ps.png",
   HiddenImage: "",
   PriceIncrease: 10,
   PayOffIncrease: 2,
